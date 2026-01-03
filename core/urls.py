75f7a92732ef316a418a37285ffe73878c6c26dd
Pleasant <johnsonpleasant02@gmail.com>
2026-01-04 00:34:50 +0100

    feat: Implement initial website structure with base template, core pages, and service templates.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
     path('about/', views.about, name='about'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
